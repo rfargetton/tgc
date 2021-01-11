00281d28f74a4615ef759f38064b831b98df2035
rfargetton <romaric.fargetton@gmail.com>
2021-01-11 11:51:09 +0100

    SEO improvements

--- components/header/index.js
+++ components/header/index.js
@@ -6,16 +6,22 @@ import Link from "next/link" ;
 export default function Header({ siteName }) {
   return (
     <header className={styles.header}>
-      <nav className={`${styles.headerContainer} ${utilsStyles.container}`}>
-        <div>
+      <nav className={`${styles.container} ${utilsStyles.container}`}>
+        <div className={styles.brand}>
           <Link href="/">
-            <a><strong>{siteName}</strong></a>
+            <a><strong>{site_name}</strong></a>
           </Link>
         </div>
-        <div className={styles.headerLinks}>
-          <Link href="/"><a>About</a></Link>
-          <Link href="/"><a>Contact</a></Link>
-        </div>
+        <div className={styles.links}>
+          <Link href="/"><a>Page 1</a></Link>
+          <Link href="/"><a>Page 2</a></Link>
+          <Link href="/"><a>Page 3</a></Link>
+          <Link href="/"><a>Page 4</a></Link>
+          <Link href="/"><a>Page 5</a></Link>
+          <Link href="/"><a>Page 6</a></Link>
+          <Link href="/"><a>Page 7</a></Link>
+          <Link href="/"><a>Page 8</a></Link>
+          </div>
       </nav>
     </header>
   )
