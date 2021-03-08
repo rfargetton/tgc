5ff8969c40e2eb0fe02809c3325ab513c35e3227
rfargetton <romaric.fargetton@gmail.com>
2021-03-08 16:29:09 +0100

    image component adjustment

--- components/content-block/index.js
+++ components/content-block/index.js
@@ -15,17 +15,18 @@ export default function ContentBlock({blocks}){
           </div>
           <div className="inner-spacing">
             {blocks.map((block, index) => (
-              <div key={block.title} className="w-full md:w-10/12 mx-auto my-10">
-                <div className={`flex justify-between items-center ${isEven(index) ? "flex-row" : "flex-row-reverse"} -m-3`}>
-                  <div className="w-full md:w-1/2 p-3">
+              <div key={block.title} className="w-full lg:w-10/12 mx-auto my-10">
+                <div className={`flex justify-between items-center ${isEven(index) ? "flex-row" : "flex-row-reverse"} -m-6`}>
+                  <div className="w-full md:w-1/2 m-3">
                     <h3 className="text-automne">{block.title}</h3>
                     <p>{block.description}</p>
                   </div>
-                  <div className="w-full md:w-80 h-80 relative m-3 rounded-xl overflow-hidden shadow-xl">
+                  <div className="w-full md:w-1/3 m-3">
                     <Image 
                       src={block.image}
-                      layout="fill"
-                      objectFit="cover"
+                      width="400"
+                      height="400"
+                      className="rounded-xl"
                     />
                   </div>
                 </div>
--- content/pages/evenement.md
+++ content/pages/evenement.md
@@ -9,19 +9,19 @@ concept_blocks:
     description: "La trace à 70% gravel sera variée avec des chemins de vigne, des
       forêts domaniales, du sable, des sous-bois, des singles, des petites
       routes avec de l’herbe au milieu et bien sûr du gravier ! "
-    image: /images/uploads/galerie-photos-3.jpg
+    image: /images/uploads/trace.jpg
   - title: Prépare-toi à en prendre plein les mirettes
     description: "La Touraine est une région chargée d'Histoire et riche en
       patrimoine qui se prête magnifiquement à la pratique du gravel. Les
       parcours de cette édition te feront découvrir parmi les plus beaux
       châteaux et villages des vals de Loire, du Cher et de l’Indre. "
-    image: /images/uploads/dsc00554.jpg
+    image: /images/uploads/patrimoine.jpg
   - title: Le vélo et les copains d’abord
     description: L’essence même de notre épreuve est de rouler en toute liberté dans
       la nature, juste çà et d’y prendre plaisir avec un bon état d’esprit et
       dans une bonne ambiance. Ici pas de compétition et encore moins de
       classement.
-    image: /images/uploads/galerie-photos-11.jpg
+    image: /images/uploads/copains.jpg
 options:
   - description: "Épreuve du dimanche seule "
     price: 30
