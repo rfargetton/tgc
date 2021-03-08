229ad19a37f3b41c2a1b119c7074b95f58d23574
rfargetton <romaric.fargetton@gmail.com>
2021-03-08 16:33:59 +0100

    fixed display property

--- components/hero/index.js
+++ components/hero/index.js
@@ -7,10 +7,10 @@ export default function Hero({image, intro, children}){
       <div className="text-gravier-light bg-feuillage">
         <div className="container">
           <div className="relative">
-            <div className="p-6 md:p-12 block md:absolute z-20 h-full w-full md:w-10/12 flex flex-col justify-center items-start">
+            <div className="p-6 md:p-12 md:absolute z-20 h-full w-full md:w-10/12 flex flex-col justify-center items-start">
               {children}
             </div>
-            <div className="h-136 ml-auto w-full md:w-9/12 relative rounded-xl overflow-hidden">
+            <div className="h-136 ml-auto w-full md:w-9/12 relative rounded-0 md:rounded-xl overflow-hidden">
               <div className="absolute z-10 top-0 bottom-0 w-4/12 bg-gradient-to-r from-black to-transparent" ></div>
               <Image 
                 src={image}
