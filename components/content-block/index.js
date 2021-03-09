f23e10a9c41008a1d1ad9960cb36a21c5be09cda
rfargetton <romaric.fargetton@gmail.com>
2021-03-09 17:53:30 +0100

    vertical spacing

--- components/content-block/index.js
+++ components/content-block/index.js
@@ -13,21 +13,23 @@ export default function ContentBlock({blocks}){
           <div className="inner-spacing relative z-20">
             <h2 className="text-center"><span className="section-title bg-automne">L'esprit TGC</span></h2>
           </div>
-          <div className="inner-spacing">
+          <div>
             {blocks.map((block, index) => (
-              <div key={block.title} className="w-full lg:w-10/12 mx-auto my-10">
-                <div className={`flex justify-between items-center ${isEven(index) ? "flex-row" : "flex-row-reverse"} -m-6`}>
-                  <div className="w-full md:w-1/2 m-3">
-                    <h3 className="text-automne">{block.title}</h3>
-                    <p>{block.description}</p>
-                  </div>
-                  <div className="w-full md:w-1/3 m-3">
-                    <Image 
-                      src={block.image}
-                      width="400"
-                      height="400"
-                      className="rounded-xl"
-                    />
+              <div className="inner-spacing">
+                <div key={block.title} className="w-full lg:w-10/12 mx-auto">
+                  <div className={`flex justify-between items-center flex-col-reverse ${isEven(index) ? "md:flex-row" : "md:flex-row-reverse"} -m-3`}>
+                    <div className="max-w-full md:w-1/2 m-3">
+                      <h3 className="text-automne">{block.title}</h3>
+                      <p>{block.description}</p>
+                    </div>
+                    <div className="max-w-full md:w-1/3 m-3">
+                      <Image 
+                        src={block.image}
+                        width="400"
+                        height="400"
+                        className="rounded-xl"
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
