31f26703bd07dea6486c5ef84fa34c4458cfd06e
rfargetton <romaric.fargetton@gmail.com>
2021-03-15 10:05:56 +0100

    key prop

--- components/content-block/index.js
+++ components/content-block/index.js
@@ -15,8 +15,8 @@ export default function ContentBlock({blocks}){
           </div>
           <div>
             {blocks.map((block, index) => (
-              <div className="inner-spacing">
-                <div key={block.title} className="w-full lg:w-10/12 mx-auto">
+              <div key={block.title} className="inner-spacing">
+                <div className="w-full lg:w-10/12 mx-auto">
                   <div className={`flex justify-between items-center flex-col-reverse ${isEven(index) ? "md:flex-row" : "md:flex-row-reverse"} -m-3`}>
                     <div className="max-w-full md:w-1/2 m-3">
                       <h3 className="text-automne">{block.title}</h3>
